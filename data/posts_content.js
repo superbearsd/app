5e248dc794cf8f030ddd533136ace919542d1868
xiongchao <253718750@qq.com>
2019-12-17 16:05:16 +0800

    tijiao

---
+++ data/posts_content.js
@@ -0,0 +1,23 @@
+var local_database = [{
+  post_body: "中华绒螯蟹常穴居江、河、湖荡泥岸，昼匿夜出，以动物尸体或谷物为食。",
+  post_title: "正是霞飞蟹黄时",
+  post_date: "Nov 19 2019",
+  user_image: "/images/avatar/3.png",
+  post_image: "/images/post/crab.png",
+  collect_num: 77,
+  view_num: 124,
+  post_id:1
+}, {
+  post_body: "李安的《比利林恩的中场战事》没有想象中乒乒乓乓的大段时间的战争戏，只有一个男孩成长的历程。没有两军对垒，却并不缺乏冲突，整部电影几乎全都是在表现不同人之间的价值观冲突，林恩和凯瑟琳，菲珊，军人和普通人，国家和家庭，民主与集权，人权与主权，普世价值和宗教信仰，战争与和平。",
+  post_title: "比利林恩的中场战事",
+  post_date: "Nov 19 2019",
+  user_image: "/images/avatar/3.png",
+  post_image: "/images/post/bl.png",
+  collect_num: 77,
+  view_num: 124,
+  post_id:2
+}]
+
+module.exports={
+  postlist:local_database
+}
